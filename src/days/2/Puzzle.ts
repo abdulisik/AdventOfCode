db05fd85bd7a09cf71d71a960262198f70647a92
Abdul Isik <aisik@dotdash.com>
2024-12-02 01:46:59 -0600

    Solve day 2

--- src/days/2/Puzzle.ts
+++ src/days/2/Puzzle.ts
@@ -1,13 +1,68 @@
 const first = (input: string) => {
-  return 'solution 1';
+  let safeCount = 0;
+  input
+    .trim()
+    .split('\n')
+    .forEach((line) => {
+      const level: number[] = line.split(/\s+/).map(Number);
+      if (isSafeReport(level)) {
+        safeCount++;
+      }
+    });
+  return safeCount;
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = '2';
+
+const isSafeReport = (level: number[]) => {
+  const direction = level[1] - level[0];
+  if (direction === 0) {
+    return false;
+  }
+  const lowerBoundary = direction > 0 ? 1 : -3;
+  const upperBoundary = direction > 0 ? 3 : -1;
+
+  for (let i = 1; i < level.length; i++) {
+    const diff = level[i] - level[i - 1];
+    if (diff < lowerBoundary || diff > upperBoundary) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const checkSafety = (level: number[]): boolean => {
+  // If the report is already safe, return true
+  if (isSafeReport(level)) {
+    return true;
+  }
+
+  // Simulate removing each level
+  for (let i = 0; i < level.length; i++) {
+    const modified = level.slice(0, i).concat(level.slice(i + 1));
+    if (isSafeReport(modified)) {
+      return true;
+    }
+  }
+
+  return false;
+};
 
 const second = (input: string) => {
-  return 'solution 2';
+  let safeCount = 0;
+  input
+    .trim()
+    .split('\n')
+    .forEach((line) => {
+      const level: number[] = line.split(/\s+/).map(Number);
+      if (checkSafety(level)) {
+        safeCount++;
+      }
+    });
+  return safeCount;
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = '4';
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
